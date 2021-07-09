ba5741081bf35a06813aeaf730827322f037bf20
BeaQ17 <beaq235@gmail.com>
2021-07-09 10:01:02 +0200

    did routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/characters', function () {
+    return view('Characters Page');
+});
+
+
+
+/* Comics route */
+Route::get('/comics', function () {
+    return view('Comics Page');
+});
+
+
+
+Route::get('/movies', function () {
+    return view('Movies Page');
+});
+
+Route::get('/tv', function () {
+    return view('TV Page');
+});
+
+Route::get('/games', function () {
+    return view('Games Page');
+});
+
+Route::get('/news', function () {
+    return view('News Page');
+});
+
+Route::get('/shop', function () {
+    return view('Shop Page');
 });
