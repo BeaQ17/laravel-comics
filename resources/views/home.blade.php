213288b9f9f278c8e651e97da860acba4b6df45f
BeaQ17 <beaq235@gmail.com>
2021-07-09 17:50:36 +0200

    css issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>DC Comics</title>
 </head>
 <body>
-    il mio documento
-    comics
+    <header>
+        <div class="blue_stripe">
+            <div class="container">
+                DC POWER VISA ®
+                ADDITIONAL DC SITES V
+            </div>
+        </div>
+        <div class="white">
+            <div class="container">
+                <img src="../laravel-comics/images/dc-logo.png" alt="DC logo" class="logo">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+                <div class="search"></div>
+            </div>
+        </div>
+
+        <div class="jumbotron">
+            <img src="../laravel-comics/images/jumbotron.jpg" alt="jumbotron image">
+        </div>
+
+    </header>
 </body>
 </html>
